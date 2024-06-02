10f56b18688fbbd680c91072b984953de336b565
mrbikoun <josephfouda10@gmail.com>
2024-06-02 17:11:49 +0100

    Update urls.py
    Added URLs for update and delete (class,user,course,teacher)

--- api/urls.py
+++ api/urls.py
@@ -21,4 +21,49 @@ urlpatterns = [
         views.TeacherListCreateView.as_view(),
         name="teacher-list-create",
     ),
+    path(
+        "teacher/",
+        views.TeacherListCreateView.as_view(),
+        name="teacher-list-create",
+    ),
+    path(
+        "teachers/<int:pk>/update/",
+        views.TeacherUpdateView.as_view(),
+        name="teacher-update",
+    ),
+    path(
+        "teachers/<int:pk>/delete/",
+        views.TeacherDeleteView.as_view(),
+        name="teacher-delete",
+    ),
+    path(
+        "class/<int:pk>/update/",
+        views.ClassUpdateView.as_view(),
+        name="class-update",
+    ),
+    path(
+        "class/<int:pk>/delete/",
+        views.ClassDeleteView.as_view(),
+        name="class-delete",
+    ),
+    path(
+        "course/<int:pk>/update/",
+        views.CourseUpdateView.as_view(),
+        name="course-update",
+    ),
+    path(
+        "course/<int:pk>/delete/",
+        views.CourseDeleteView.as_view(),
+        name="course-delete",
+    ),
+    path(
+        "user/<int:pk>/update/",
+        views.UserUpdateView.as_view(),
+        name="user-update",
+    ),
+    path(
+        "user/<int:pk>/delete/",
+        views.UserDeleteView.as_view(),
+        name="user-delete",
+    ),
 ]
